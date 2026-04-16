46911e2910050f6696f30895a301e473e7315718
Michael Wu <michaelmwu@gmail.com>
2026-04-16 15:33:00 +0900

    Switch guide map to Google Maps by default

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly GOOGLE_MAPS_API_KEY?: string;
+  readonly GOOGLE_MAPS_JS_API_KEY?: string;
+  readonly PUBLIC_GOOGLE_MAPS_API_KEY?: string;
+  readonly PUBLIC_MAP_PROVIDER?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
